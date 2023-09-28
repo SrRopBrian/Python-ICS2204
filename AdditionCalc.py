ad4eeb52123f2eff9a7b5a679d3bb398f93566da
SrRopBrian <kipngenobrian070@gmail.com>
2023-09-28 21:18:17 +0300

    Simple Addition Calculator in Python

---
+++ AdditionCalc.py
@@ -0,0 +1,11 @@
+#simple calculator that performs additions of two integers
+
+name = input("Please enter your name: ")
+print("Hello " + name)
+print()
+
+num1 = int(input("Please enter number 1: "))
+num2 = int(input("Please enter number 2: "))
+print()
+
+print(name, " , the sum of ", num1, " and ", num2, " is ", (num1+num2))
